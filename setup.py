9f1d9406d0c8724ecf110b362c0e715826dfefa3
Greg Short <shortg29@gmail.com>
2022-05-09 08:10:52 +0900

    add setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='zenkai',
+    version='0.1.0',
+    packages=['zenkai']
+)
